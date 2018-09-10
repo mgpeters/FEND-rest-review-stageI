5213590639fd2bb90171612f9648a070024492ff
Matt Peters <matthew.george.peters@gmail.com>
2018-09-10 17:46:48 -0400

    added mapbox api token

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: '<your MAPBOX API KEY HERE>',
+    mapboxToken: 'pk.eyJ1IjoibWdwZXRlcnMiLCJhIjoiY2psd3RmdnN0MDNudDNwbWc4ZHBqanNkcSJ9.Ek_4d_-gtFRPQ5eEml133Q',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
