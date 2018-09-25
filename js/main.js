440d168ab9c863409ebb031e07dc2ff377f9f384
Matt Peters <matthew.george.peters@gmail.com>
2018-09-24 20:51:13 -0400

    working accessability, added tab index" "

--- js/main.js
+++ js/main.js
@@ -194,7 +194,8 @@ addMarkersToMap = (restaurants = self.restaurants) => {
   restaurants.forEach(restaurant => {
     // Add marker to the map
     const marker = DBHelper.mapMarkerForRestaurant(restaurant, self.newMap);
-    marker.on("click", onClick);
+    marker.setAttribute('tabIndex','-1');
+    marker.on("click", onClick);        
     function onClick() {
       window.location.href = marker.options.url;
     }
